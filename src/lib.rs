7ffb60139eb71674c48707c17251b37a6681f360
alfianlensun <alfianricky46@gmail.com>
2023-05-29 13:28:49 +0800

    add get printers by name

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,11 @@ pub fn get_printers() -> String {
 
     panic!("Unsupported OS");
 }
+
+pub fn get_printers_by_name(name: String) -> String {
+    if cfg!(windows) {
+        return windows::get_printers_by_name(name);
+    }
+
+    panic!("Unsupported OS");
+}
--- src/windows.rs
+++ src/windows.rs
@@ -13,3 +13,17 @@ pub fn get_printers() -> String {
     let out_str = str::from_utf8(&command.stdout).unwrap();
     return out_str.to_string();
 }
+
+
+/**
+ * Get printers by name on windows using powershell
+ */
+pub fn get_printers_by_name (name: String) -> String {
+    let command = Command::new("powershell")
+        .arg("-Command")
+        .arg(format!("Get-Printer -Name {} | ConvertTo-Json", name))
+        .output()
+        .unwrap();
+    let out_str = str::from_utf8(&command.stdout).unwrap();
+    return out_str.to_string();
+}
